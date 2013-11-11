77e6e009ce4abebafcad9f01c819709ade8577df
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-11 16:38:06 -0600

    load gif added

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   $("#gettweets").submit(function( event ){
     event.preventDefault();
+    $( "#loader").css("display", "block");
     // var $newTweetName = $(this);
     // var data = $newTweetName.serialize();
     var url = $( "#handle" ).val();
     $.get('/' + url, function(response){
-      $('.response').html(response);
+      $('.response').html('').html(response);
+      var loader = "<img src=\"image\/ajax-loader.gif\" style=\"display: none;\" id=\"loader\">";
+
+      $( ".response" ).append(loader)
     });
+
+    
 
   });
 });
